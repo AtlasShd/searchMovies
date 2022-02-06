cb1c28cb5c5d8ad3d6b598a17637760d23a52d13
AtlasShd <tlsshrugged@gmail.com>
2022-02-06 18:59:09 +0300

    finished welcome section with search

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 'use strict';
 
-const coreBody = document.querySelector('.core__body');
+const coreBody = document.querySelector('.core__body'),
+	welcomeWallpaper = document.querySelector('.welcome__wallpaper');
 
 //add cards to page
 class card {
 
-	constructor(src, title, average, count) {
+	constructor(src, title, average, count, overview) {
 		this.src = src;
 		this.title = title;
 		this.average = average;
+		this.overview = overview;
 		this.reduceNumber(count);
 		this.color = this.averageColor();
 	}
@@ -62,6 +64,63 @@ class card {
 	}
 }
 
+class welcome {
+
+	constructor(src, title, average, count, year, overview) {
+		this.src = src;
+		this.title = title;
+		this.average = average;
+		this.year = String(year).slice(0, 4);
+		this.count = count;
+		this.overview = overview;
+		this.color = this.averageColor();
+	}
+
+	averageColor() {
+		if (this.average >= 8) {
+			return 'olivedrab';
+		} else if (this.average < 6) {
+			return 'firebrick';
+		} else {
+			return 'gold';
+		}
+	}
+
+	render() {
+		welcomeWallpaper.style.background = `url("https://www.themoviedb.org/t/p/original${this.src}") 50% 20% / cover no-repeat`;
+
+		const element = document.createElement('div');
+
+		element.classList.add('welcome__body');
+		element.innerHTML = `
+		<div class="welcome__main">
+			<div class="welcome__title">
+			${this.title}
+			</div>
+			<div class="welcome__year">
+				© ${this.year}
+			</div>
+			<div class="welcome__subtitle">
+			${this.overview}
+			</div>
+		</div>
+		<div class="welcome__footer">
+			<div class="welcome__average" style="background: ${this.color}">
+			${this.average}
+			</div>
+			<div class="welcome__count">
+			${this.count}
+			</div>
+		</div>
+		`;
+
+		welcomeWallpaper.append(element);
+	}
+}
+
+let welcomeIsTrue = true;
+
+
 //get data from API
 function cardsCount(query = '') {
 	const number = Math.floor(document.documentElement.clientWidth / 335) * 4; //335 = card width + column gap
@@ -71,6 +130,10 @@ function cardsCount(query = '') {
 		fetch(url)
 			.then(res => res.json())
 			.then(data => {
+				if (welcomeIsTrue) {
+					createWelcome(data);
+					welcomeIsTrue = false;
+				}
 				tempNumber = ((number - (20 * i)) >= 20) ? 20 : number % 20;
 				createCards(data, tempNumber);
 			});
@@ -91,11 +154,22 @@ function createCards(data, temp) {
 	}
 }
 
+function createWelcome(data) {
+	const movie = data.results[0];
+	new welcome(movie.poster_path, movie.title, movie.vote_average, movie.vote_count, movie.release_date, movie.overview).render();
+}
+
 cardsCount();
 
 //search movies
 document.querySelector('.header__search').addEventListener('submit', (e) => {
 	e.preventDefault();
+	welcomeIsTrue = true;
+	welcomeWallpaper.innerHTML = `
+		<div class="welcome__header">
+			THE MOST SUITABLE
+ 		</div>
+	`;
 	coreBody.innerHTML = '';
 	cardsCount(document.querySelector('.header__input').value);
 });
