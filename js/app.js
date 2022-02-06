582f1a80e6c2f2c50f7b3ad70385e6aef0dc4fb8
AtlasShd <tlsshrugged@gmail.com>
2022-02-06 15:59:13 +0300

    add API cards of movies

--- js/app.js
+++ js/app.js
@@ -1,19 +1,85 @@
 'use strict';
 
+//add cards to page
 class card {
 
-	constructor(img, alt) {
-		this.img = img;
-		this.img = img;
+	constructor(src, title, average, count) {
+		this.src = src;
+		this.title = title;
+		this.average = average;
+		this.reduceNumber(count);
+	}
+
+	reduceNumber(num) {
+		const leng = String(num).length;
+
+		if (leng >= 5) {
+			this.count = (Math.round(num / 1000)) + 'K';
+		} else if (leng === 4) {
+			this.count = (Math.round(num / 100) / 10) + 'K';
+		} else {
+			this.count = num;
+		}
+	}
+
+	averageColor() {
+		
 	}
 
 	render() {
 		const element = document.createElement('div');
+
+		element.classList.add('card');
+		element.innerHTML = `
+			<div class="card__wallpaper">
+				<img class="card__img" src="https://www.themoviedb.org/t/p/w600_and_h900_face${this.src}" alt="${this.title}" loading="lazy">
+			</div>
+			<div class="card__footer">
+				<div class="card__title">
+					${this.title}
+				</div>
+				<div class="card__score">
+		 			<div class="card__average">
+						${this.average}
+					</div>
+					<div class="card__count">
+						${this.count}
+					</div>
+				</div>
+			</div>
+		`;
+
+		document.querySelector('.core__body').append(element);
 	}
 }
 
-// disabled transition bedore loading page
 
+//get data from API
+function cardsCount() {
+	const number = Math.floor(document.documentElement.clientWidth / 335) * 4; //335 = card width + column gap
+	let tempNumber;
+	for (let i = 0; i < number / 20; i++) {
+		const url = `
+		https://api.themoviedb.org/3/discover/movie?page=${i + 1}&sort_by=popularity.desc&api_key=4aff51c20dec81f753b4cac50ca60b49`;
+		fetch(url)
+			.then(res => res.json())
+			.then(data => {
+				tempNumber = ((number - (20 * i)) >= 20) ? 20 : number % 20;
+				createCards(data, tempNumber);
+			});
+	}
+}
+
+function createCards(data, temp) {
+	for (let i = 0; i < temp; i++) {
+		const movie = data.results[i];
+		new card(movie.poster_path, movie.title, movie.vote_average, movie.vote_count).render();
+	}
+}
+
+cardsCount();
+
+// disabled transition bedore loading page
 window.addEventListener('load', () => {
 	document.querySelector('#transition-none').removeAttribute('id');
 });
